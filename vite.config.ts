406b2d0ac9537935b9419e8473f0d5bc9d1f923e
Joost van Tiggelen <joostvantiggelen@Joosts-MacBook-Pro.local>
2025-11-05 13:18:49 +0100

    visual changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/avinstallatie/",
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
